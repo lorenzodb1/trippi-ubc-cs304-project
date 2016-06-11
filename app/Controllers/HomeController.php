9ab315fce66e76c4e07813c01abe3f59a7586cb9
lorenzodb1 <lorenzodb1@hotmail.it>
2016-06-11 16:23:31 -0700

    More views for profile homepage

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -31,22 +31,29 @@ class HomeController{
     
     public function signIn(Response $response, Request $request, Twig $view, Router $router){
 
-        $email = $request->getAttribute('email');
-        var_dump($email);
+        $data = $request->getParsedBody();
+        $email_info = '\'' . filter_var($data['email'], FILTER_SANITIZE_STRING) . '\'';
+        $password_info = $data['password'];
+        var_dump($email_info);
+        var_dump($password_info);
+        //var_dump($user_info['password']);
         //$email = $array[0];
         //$password = $request->getAttribute('password');
         $db = new Db();
-        $query = "SELECT * FROM `user` u WHERE u.email =  'bob@gmail.com'";
+        $query = "SELECT *
+                  FROM `user` u 
+                  WHERE u.email = " . $email_info;
         $result = $db->query($query);
         
         if($result){
             //check if the password is correct
-            $query2 = "SELECT a.locationID, l.city, l.country, a.rating FROM accomodation a, location l WHERE a.locationID = l.locationID and a.rating > 2";
+            $query2 = "SELECT a.locationID, l.city, l.country, a.rating 
+                       FROM accomodation a, location l 
+                       WHERE a.locationID = l.locationID AND 
+                             a.rating > 2";
 
             $tripsHotelResult = $db->query($query2);
-            return $view->render($response, 'Trips/Trips.twig', [
-                'trips'=> $tripsHotelResult
-            ]);
+            return $view->render($response, 'profile/profile.twig', ['trips'=> $tripsHotelResult]);
             
             //else we must say password is incorrect and redirect back to page..
             
@@ -54,7 +61,10 @@ class HomeController{
         else{
             //redirect and give error message saying user name does not exist
             return $response->withRedirect($router->pathFor('home'));
-//            $query2 = "SELECT a.locationID, l.city, l.country, a.rating FROM accomodation a, location l WHERE a.locationID = l.locationID and a.rating > 2";
+//            $query2 = "SELECT a.locationID, l.city, l.country, a.rating
+//                       FROM accomodation a, location l
+//                       WHERE a.locationID = l.locationID AND
+//                             a.rating > 2";
 //
 //            $tripsHotelResult = $db->query($query2);
 //            return $view->render($response, 'Trips/Trips.twig', [
