c840e28cf428a62d906026e9a8e95ea222815b59
lorenzodb1 <lorenzodb1@hotmail.it>
2016-06-13 21:11:24 -0700

    DOES NOT RUN

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -18,37 +18,34 @@ class Profile {
      * TODO: what should these functions return?
      */
     
-    public function create_profile($email, $username, $name, $hometown, $country, $dateOfBirth, $aboutMe) {
+    public static function create_profile($email, $password, $name, $hometown, $country, $dateOfBirth, $aboutMe) {
         $db = new Db();
         $query = "UPDATE `user`
                   SET `name` = " . ModelsUtils::mysqlstring($name) . ", 
                       `hometown` = " . ModelsUtils::mysqlstring($hometown) . ", `country` = " . ModelsUtils::mysqlstring($country) . ", 
-                      `dateOfBirth` = " . ModelsUtils::mysqlstring($dateOfBirth) . ", `AboutMe` = " . ModelsUtils::mysqlstring($aboutMe) . " 
-                  WHERE `email` = " . ModelsUtils::mysqlstring($email);
+                      `dateOfBirth` = " . ModelsUtils::mysqlstring($dateOfBirth) . ", `aboutMe` = " . ModelsUtils::mysqlstring($aboutMe) . " 
+                  WHERE `email` = " . ModelsUtils::mysqlstring($email) . " AND 
+                        `password` = " . ModelsUtils::mysqlstring(crypt($password, '$6$rounds=5000$' . $email . '$'));
         $result = $db->query($query);
-        return true;
+        return $result;
     }
 
-    public function update_profile($email, $password, $data_type, $new_data) {
+    public static function update_profile($email, $password, $data_type, $new_data) {
         $db = new Db();
         $query = "UPDATE `user`
                   SET " . ModelsUtils::mysqlstring($data_type) . " = " . ModelsUtils::mysqlstring($new_data) . " 
                   WHERE `email` = " . ModelsUtils::mysqlstring($email) . " AND 
                         `password` = " . ModelsUtils::mysqlstring(crypt($password, '$6$rounds=5000$' . $email . '$'));
         $result = $db->query($query);
-        return true;
+        return $result;
     }
 
-    public function delete_profile($email, $password) {
+    public static function delete_profile($email, $password) {
         $db = new Db();
         $query = "DELETE FROM `user` 
                   WHERE `email` = " . ModelsUtils::mysqlstring($email) . " AND 
                         `password` = " . ModelsUtils::mysqlstring(crypt($password, '$6$rounds=5000$' . $email . '$'));
         $result = $db->query($query);
-        if($result) {
-            return true;
-        } else {
-            return false;
-        }
+        return $result;
     }
 }
